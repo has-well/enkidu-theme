484be6b5a4afc41a60fbc23ea36f7a442a7027ee
neries <0neries0@gmail.com>
2022-04-14 00:12:07 +0300

    upd post view

--- single.php
+++ single.php
@@ -7,9 +7,9 @@ if (have_posts()) :
     while (have_posts()) : the_post(); ?>
 
         <!-- post content-->
-        <section id="post">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <div class="container">
+            <div class="container p-6">
 
                 <div class="flex justify-center">
 
@@ -21,8 +21,8 @@ if (have_posts()) :
                             <div class="post-info flex items-center">
                                 <div class="img-fake rounded-full bg-gray-400 w-12 h-12 mr-5"></div>
                                 <div>
-                                    <a class="post-author inline-block">By <?php echo get_the_author() ?></a>
-                                    <div class="post-date text-sm text-gray-600"><?php the_date() ?></div>
+                                    <a class="post-author block">By <?php echo get_the_author() ?></a>
+                                    <time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="post-date text-sm text-gray-600"><?php the_date() ?></time>
                                 </div>
                             </div>
 
@@ -37,7 +37,7 @@ if (have_posts()) :
                 </div>
 
             </div>
-        </section>
+        </article>
 
     <?php endwhile;
 endif;
