5b3df5e294d086c6453f40d39f7a94e3ca4c37c9
neries <0neries0@gmail.com>
2022-04-13 23:00:52 +0300

    AAGyCAxk: right header nav style nQ9rFNXv: temp remove lang switcher

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <header class="sticky top-0 py-2 bg-white">
     <div class="container overflow-vs">
         <nav class="relative z-50 h-14 md:h-24 select-none">
-            <div class="container overflow-vs relative flex flex-wrap items-center justify-between h-14 md:h-24 mx-auto font-medium md:overflow-visible px-4 sm:px-4 md:px-2">
+            <div class="container overflow-vs relative flex flex-wrap items-center justify-between h-14 md:h-24 mx-auto font-medium md:overflow-visible px-4 md:px-0">
                 <div class="flex items-center justify-start h-full mr-4">
                     <a href="<?php echo home_url() ?>" class="inline-block py-4 md:py-0">
                         <img class="h-5 sm:h-7" src="<?php echo $baseURL ?>/assets/img/logo.svg"
@@ -25,7 +25,7 @@
                 </div>
                 <div id="main_nav" class="top-0 left-0 items-start invisible lg:visible w-full h-full p-0 md:p-4 text-sm bg-opacity-50 md:items-center w-2/3 lg:w-3/4 xl:w-10/12 md:absolute lg:text-base md:bg-transparent md:p-0 md:relative md:flex">
                     <div id="main_menu" class="flex-col w-full h-auto overflow-hidden bg-white rounded-lg md:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row">
-                        <div class="flex flex-col items-start w-full space-x-0 md:space-x-4 text-center lg:space-x-8 md:w-2/3 md:mt-0 md:flex-row md:items-center">
+                        <div class="flex flex-col items-start w-full space-x-0 md:space-x-4 text-center lg:space-x-8 md:w-1/2 md:mt-0 md:flex-row md:items-center">
                             <?php
                                 $menu = wp_get_nav_menu_items('head_left', array());
                                 if ($menu) {
@@ -42,18 +42,18 @@
                             ?>
                         </div>
 
-                        <div class="flex flex-col lg:flex-row items-center text-center justify-between w-full pt-4 items-center md:w-1/2 md:py-0">
+                        <div class="flex flex-col lg:flex-row items-center text-center justify-center pt-4 md:py-0 md:ml-auto">
                             <?php if ($headRightMenuItems = wp_get_nav_menu_items('head_right')): ?>
                             <?php foreach ($headRightMenuItems as $menuItem): ?>
-                                <a class="w-full text-sm lg:text-lg font-bold text-blue md:px-0 my-2 lg:px-3 md:w-auto hover:text-main_yellow transition-colors"
+                                <a class="btn btn-yellow md:mr-5 px-6 font-bold my-2"
                                    href="<?php echo esc_url($menuItem->url) ?>">
                                     <?php echo $menuItem->title ?>
                                 </a>
                             <?php endforeach; ?>
                             <?php endif; ?>
 
-                            <a href="<?php the_field('login_link'); ?>" class="btn btn-yellow font-bold my-2"><?php _e( 'Log in', 'enkidu-v2' ); ?></a>
-                            <?php pll_the_languages(array('dropdown'=>1, 'display_names_as'=>'slug'));  ?>
+<!--                            <a href="--><?php //the_field('login_link'); ?><!--" class="btn btn-yellow font-bold my-2">--><?php //_e( 'Log in', 'enkidu-v2' ); ?><!--</a>-->
+<!--                            --><?php //pll_the_languages(array('dropdown'=>1, 'display_names_as'=>'slug'));  ?>
                         </div>
                     </div>
                 </div>
